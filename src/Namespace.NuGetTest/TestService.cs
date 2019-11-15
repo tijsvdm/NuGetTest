832f195b66b5c1821f9b4f432fd810b11409e93e
Tijs Vandormael <tijs.vandormael@empyrean-apac.com>
2019-11-15 14:09:55 +0700

    Add GetTimeZone method

--- src/Namespace.NuGetTest/TestService.cs
+++ src/Namespace.NuGetTest/TestService.cs
@@ -8,12 +8,24 @@ namespace Empyrean.ApprovalApiClient
     public static class TestService
     {
         /// <summary>
-        /// Gets the time.
+        /// Gets the current UTC time.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>
+        /// The current UTC time.
+        /// </returns>
         public static DateTime GetTime()
         {
             return DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Gets the time zone for a given date time.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns>The time zone</returns>
+        public static string GetTimeZone(DateTime dateTime)
+        {
+            return dateTime.Kind.ToString();
         }
     }
 }
